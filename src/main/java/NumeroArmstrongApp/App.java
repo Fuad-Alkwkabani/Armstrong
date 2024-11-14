a26d01deb2a88ab884e0baf09011126a7a2527f2
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-11-14 16:22:18 +0100

    creat the frist step of the app

---
+++ src/main/java/NumeroArmstrongApp/App.java
@@ -0,0 +1,13 @@
+package NumeroArmstrongApp;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce un número para verificar si es un número de Armstrong: ");
+        int numero = scanner.nextInt();
+
+    }
+}
---
+++ src/main/java/NumeroArmstrongApp/ArmstrongCheck.java
@@ -0,0 +1,9 @@
+package NumeroArmstrongApp;
+
+public class ArmstrongCheck{
+    private int numero;      
+    
+
+   
+    
+}
